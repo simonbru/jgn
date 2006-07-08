9c619fd79c2801e773bd9a81b4b2635da6f7a4f6
mhicks <mhicks@a42d04ac-5654-0410-88ef-dd6235ab3d57>
2006-07-08 20:48:57 +0000

    * Initial contribution of JGN 2.0 alpha

---
+++ jgn/trunk/trunk/src/com/captiveimagination/jgn/UDPMessageServer.java
@@ -0,0 +1,167 @@
+/**
+ * Copyright (c) 2005-2006 JavaGameNetworking
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'JavaGameNetworking' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Created: Jul 6, 2006
+ */
+package com.captiveimagination.jgn;
+
+import java.io.*;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.*;
+
+import com.captiveimagination.jgn.message.*;
+
+/**
+ * @author Matthew D. Hicks
+ */
+public class UDPMessageServer extends NIOMessageServer {
+	private DatagramChannel channel;
+	private ByteBuffer readLookup;
+	
+	public UDPMessageServer(SocketAddress address) throws IOException {
+		this(address, 1024);
+	}
+	
+	public UDPMessageServer(SocketAddress address, int maxQueueSize) throws IOException {
+		super(address, maxQueueSize);
+		readLookup = ByteBuffer.allocateDirect(1024 * 10);
+	}
+
+	protected SelectableChannel bindServer(SocketAddress address) throws IOException {
+		channel = selector.provider().openDatagramChannel();
+		channel.socket().bind(address);
+		channel.configureBlocking(false);
+		channel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+		return channel;
+	}
+	
+	protected void accept(SelectableChannel channel) throws IOException {
+		// UDP Message Server will never receive an accept event
+	}
+
+	protected void connect(SelectableChannel channel) throws IOException {
+		// UDP Message Server will never receive a connect event
+	}
+
+	protected void read(SelectableChannel c) throws IOException {
+		MessageClient client = null;
+		try {
+			InetSocketAddress address = (InetSocketAddress)channel.receive(readLookup);
+			readLookup.limit(readLookup.position());
+			readLookup.position(0);
+			client = getMessageClient(address);
+			if (client == null) {
+				client = new MessageClient(address, this);
+				client.setStatus(MessageClient.STATUS_NEGOTIATING);
+				getIncomingConnectionQueue().add(client);
+				getMessageClients().add(client);
+			}
+			
+			client.getReadBuffer().put(readLookup);
+			readLookup.clear();
+			
+			Message message;
+			while ((message = readMessage(client)) != null) {
+				client.getIncomingMessageQueue().add(message);
+			}
+		} catch(IOException exc) {
+			// Handle connections being closed
+			if (client != null) disconnectInternal(client, false);
+		} catch(MessageHandlingException exc) {
+			// FIXME we need to show the cause!
+			// appearantly IOE is not suitable for this
+			throw new RuntimeException(exc);
+		}
+	}
+
+	protected boolean write(SelectableChannel c) throws IOException {
+		for (MessageClient client : getMessageClients()) {
+			if (client.getCurrentWrite() != null) {
+				client.sent();		// Let the system know something has been written
+				channel.send(client.getCurrentWrite().getBuffer(), client.getAddress());
+				if (!client.getCurrentWrite().getBuffer().hasRemaining()) {
+					// Write all messages in combined to sent queue
+					client.getCurrentWrite().process();
+					
+					client.setCurrentWrite(null);
+				} else {
+					// Take completed messages and add them to the sent queue
+					client.getCurrentWrite().process();
+				}
+			} else {
+				CombinedPacket combined;
+				try {
+					// TODO make 50000 adjustable (getter/setter)
+					combined = PacketCombiner.combine(client, 50000);
+				} catch (MessageHandlingException exc) {
+					// FIXME handle properly
+					exc.printStackTrace();
+					combined = null;
+				}
+
+				if (combined != null) {
+					channel.send(combined.getBuffer(), client.getAddress());
+					if (combined.getBuffer().hasRemaining()) {
+						client.setCurrentWrite(combined);
+						
+						// Take completed messages and add them to the sent queue
+						combined.process();
+						
+						return false;	// No more room for sending
+					} else {
+						client.setCurrentWrite(null);
+						
+						// Write all messages in combined to sent queue
+						combined.process();
+						
+						return true;
+					}
+				} else if (client.getStatus() == MessageClient.STATUS_DISCONNECTING) {
+					disconnectInternal(client, true);
+				}
+			}
+		}
+		return false;
+	}
+
+	public MessageClient connect(SocketAddress address) throws IOException {
+		MessageClient client = getMessageClient(address);
+		if (client != null) {
+			return client;		// Client already connected, simply return it
+		}
+		client = new MessageClient(address, this);
+		client.setStatus(MessageClient.STATUS_NEGOTIATING);
+		getMessageClients().add(client);
+		client.sendMessage(JGN.generateRegistrationMessage());
+		return null;
+	}
+}
---
+++ jgn/trunk/trunk/src/com/captiveimagination/jgn/convert/StringArrayConverter.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2005-2006 JavaGameNetworking
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'JavaGameNetworking' nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software 
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Created: Jun 7, 2006
+ */
+package com.captiveimagination.jgn.convert;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.nio.*;
+
+import com.captiveimagination.jgn.*;
+import com.captiveimagination.jgn.message.*;
+
+/**
+ * @author Matthew D. Hicks
+ */
+public class StringArrayConverter implements Converter {
+	public StringArrayConverter() {
+		ConversionHandler.initConverters();
+	}
+
+	public void set(Message message, Method setter, ByteBuffer buffer) throws IllegalArgumentException,
+					IllegalAccessException, InvocationTargetException {
+		int length = buffer.getInt();
+		String[] array = null;
+		if (length != -1) {
+			array = new String[length];
+			byte[] b;
+			for (int i = 0; i < length; i++) {
+				int l = buffer.getInt();
+				if (l == -1) continue;
+				b = new byte[l];
+				buffer.get(b);
+				try {
+					array[i] = new String(b, "UTF-8");
+				} catch (UnsupportedEncodingException exc) {
+					// never happens
+					exc.printStackTrace(); // just be paranoid
+				}
+			}
+		}
+		setter.invoke(message, new Object[] {array});
+	}
+
+	public void get(Message message, Method getter, ByteBuffer buffer) throws IllegalArgumentException,
+					IllegalAccessException, InvocationTargetException {
+		String[] array = (String[]) getter.invoke(message, EMPTY_ARRAY);
+		if (array == null) {
+			buffer.putInt(-1);
+		} else {
+			buffer.putInt(array.length);
+			for (String s : array) {
+				//buffer.put(b);
+				if (s == null) {
+					buffer.putInt(-1);
+				} else {
+					try {
+						byte[] b = s.getBytes("UTF-8");
+						buffer.putInt(b.length);
+						buffer.put(b);
+					} catch (UnsupportedEncodingException exc) {
+						// never happens
+						exc.printStackTrace(); // just be paranoid
+					}
+				}
+			}
+		}
+	}
+
+}

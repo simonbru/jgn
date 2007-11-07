5f9f07a38179ee9fcde5d009ff4a266ed40718a6
NateS <NateS@a42d04ac-5654-0410-88ef-dd6235ab3d57>
2007-11-07 22:25:39 +0000

    Updated javadocs.

--- core/trunk/src/com/captiveimagination/jgn/convert/type/FieldExternalizable.java
+++ core/trunk/src/com/captiveimagination/jgn/convert/type/FieldExternalizable.java
@@ -8,7 +8,7 @@ import com.captiveimagination.jgn.convert.FieldConverter;
 
 /**
  * Allows implementing classes to do additional serialization after the normal serialization done by {@link FieldConverter}. This
- * is useful when an object has a state where parts it don't need to be serialized. Note that FieldConverter will automatically
+ * is useful when an object has a state where parts of it don't need to be serialized. Note that FieldConverter will automatically
  * serialize any non-transient fields.
  * 
  * @author Nathan Sweet <misc@n4te.com>

3f9ffaf678f254eb8031edde75a02b89be686322
mhicks <mhicks@a42d04ac-5654-0410-88ef-dd6235ab3d57>
2007-12-30 21:25:49 +0000

    Fix to OrderedMessage to resolve a bug not properly assigning the orderId

--- core/trunk/src/com/captiveimagination/jgn/message/OrderedMessage.java
+++ core/trunk/src/com/captiveimagination/jgn/message/OrderedMessage.java
@@ -53,6 +53,10 @@ public abstract class OrderedMessage extends Message implements GroupMessage, Ce
 	
 	private int orderId;
 	
+	public OrderedMessage() {
+		OrderedMessage.assignOrderId(this);
+	}
+	
 	public int getOrderId() {
 		return orderId;
 	}

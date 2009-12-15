8f9d3cce81d6321e20fb9587b4dcdc7a90a4804e
hem <hem@jordefamn.se>
2009-12-15 20:37:08 +0000

    Added support for the use of the proximity function in GraphicalController in determining whether a player should be sent a create or remove message for an object.

--- core/trunk/src/com/captiveimagination/jgn/synchronization/SyncWrapper.java
+++ core/trunk/src/com/captiveimagination/jgn/synchronization/SyncWrapper.java
@@ -40,6 +40,7 @@ import com.captiveimagination.jgn.clientserver.JGNConnection;
 import com.captiveimagination.jgn.clientserver.JGNServer;
 import com.captiveimagination.jgn.synchronization.message.SynchronizeCreateMessage;
 import com.captiveimagination.jgn.synchronization.message.SynchronizeMessage;
+import com.captiveimagination.jgn.synchronization.message.SynchronizeRemoveMessage;
 
 class SyncWrapper {
 	private short id;
@@ -50,8 +51,19 @@ class SyncWrapper {
 	private SynchronizeCreateMessage createMessage;
 	private short ownerPlayerId;
 	
+	/**
+	 * Long for storing when the last update to the server was, if this is on the client
+	 */
 	private long lastUpdate;
+	/**
+	 * Map for storing when the last update to a connected client was, if this is on the server
+	 */
 	private HashMap<Short, Long> lastServerUpdate = new HashMap<Short, Long>();
+	/**
+	 * Map for storing the last action of create/remove for a connected client
+	 * 0 = nothing; 1 = sent create message; 2 = sent remove message
+	 */
+	private HashMap<Short, Short> lastAction = new HashMap<Short, Short>();
 	
 	public SyncWrapper(Object object, long rate, SynchronizeCreateMessage createMessage, short ownerPlayerId) {
 		if (object == null) throw new RuntimeException("Object is null: " + id);
@@ -95,6 +107,36 @@ class SyncWrapper {
 		return ownerPlayerId;
 	}
 	
+	/**
+	 * Removes this key from the lastServerUpdate map
+	 * @param playerId the key to remove
+	 * @return the object referenced by this key or null if the key didn't exist
+	 */
+	public Object removeLastServerUpdateKey(short playerId) {
+		return lastServerUpdate.remove(playerId);
+	}
+	
+	/**
+	 * Removes this key from the lastAction map
+	 * @param playerId the key to remove
+	 * @return the object referenced by this key or null if the key didn't exist
+	 */
+	public Object removeLastActionKey(short playerId) {
+		return lastAction.remove(playerId);
+	}
+	
+	public boolean hasLastAction(short playerId) {
+		return lastAction.containsKey(playerId);
+	}
+	
+	public short getLastAction(short playerId) {
+		return lastAction.get(playerId);
+	}
+	
+	public void setLastAction(short playerId, short value) {
+		lastAction.put(playerId, value);
+	}
+	
 	protected void update(SynchronizationManager manager, JGNServer server, GraphicalController controller) {
 		long updateRate = rate;
 		SynchronizeMessage message = controller.createSynchronizationMessage(getObject());
@@ -102,6 +144,23 @@ class SyncWrapper {
 		message.setSyncObjectId(getId());
 		for(JGNConnection conn : server.getConnections()) {
 			if(controller.proximity(getObject(), conn.getPlayerId()) > 0f && conn.getPlayerId() != getOwnerPlayerId()) {
+				//Check if sent create message
+				if(lastAction.containsKey(conn.getPlayerId())) {
+					if(lastAction.get(conn.getPlayerId()) != 1) { //No create message sent
+						System.out.println("Sending create for "+getId());
+						lastAction.put(conn.getPlayerId(), (short)1);
+						getCreateMessage().setSyncObjectId(getId());
+						getCreateMessage().setSyncManagerId(manager.getId());
+						server.sendToPlayer(getCreateMessage(), conn.getPlayerId());
+					}
+				}
+				else {
+					System.out.println("Sending create for "+getId());
+					lastAction.put(conn.getPlayerId(), (short)1);
+					getCreateMessage().setSyncObjectId(getId());
+					getCreateMessage().setSyncManagerId(manager.getId());
+					server.sendToPlayer(getCreateMessage(), conn.getPlayerId());
+				}
 				updateRate = (long) (rate/controller.proximity(getObject(), conn.getPlayerId()));
 				if(!lastServerUpdate.containsKey(conn.getPlayerId())) {
 					lastServerUpdate.put(conn.getPlayerId(), (long)0);
@@ -112,6 +171,22 @@ class SyncWrapper {
 					}
 					
 					lastServerUpdate.put(conn.getPlayerId(), System.nanoTime());
+				}
+			}
+			else if(controller.proximity(getObject(), conn.getPlayerId()) == 0f){
+				//Check if sent remove message
+				if(lastAction.containsKey(conn.getPlayerId())) {
+					if(lastAction.get(conn.getPlayerId()) != 2) { //No remove message sent
+						System.out.println("Sending remove for "+getId());
+						lastAction.put(conn.getPlayerId(), (short)2);
+						SynchronizeRemoveMessage remove = new SynchronizeRemoveMessage();
+						remove.setSyncObjectId(getId());
+						remove.setSyncManagerId(manager.getId());
+						server.sendToPlayer(remove, conn.getPlayerId());
+					}
+				}
+				else {
+					lastAction.put(conn.getPlayerId(), (short)2);
 				}
 			}
 		}
